b9d2ba45d386e49c2400051a4105125c5153c9d7
Rahim Askarzadeh <raskarzadeh1@my.bcit.ca>
2022-05-02 13:50:56 -0700

    Removed fonts style to use system default.

--- lib/constants.dart
+++ lib/constants.dart
@@ -7,22 +7,17 @@ const kColourSidebarTile = Color(0xFF2C2C2E);
 const kColourSidebarTileText = Colors.white;
 const kColourTileDivider = Color(0xFF48484A);
 
-const kTextStyleWelcomeMessage = TextStyle(
-    fontSize: 30,
-    fontWeight: FontWeight.bold,
-    fontFamily: 'Arial',
-    color: Colors.white);
+const kTextStyleWelcomeMessage =
+    TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white);
 
 const kTextStylePlaceHelpPlaceholder = TextStyle(
   fontSize: 16,
-  fontFamily: 'Arial',
   color: Color(0xFF8E8E93),
 );
 
 const kColourLight = Color(0xFFEA938A);
 
 const kTextStyleLargeTitle = TextStyle(
-  fontFamily: 'Arial',
   color: Colors.white,
   fontWeight: FontWeight.bold,
   fontSize: 32,
@@ -30,5 +25,4 @@ const kTextStyleLargeTitle = TextStyle(
 
 const kTextStyleSidebarTile = TextStyle(
   fontSize: 16,
-  fontFamily: 'Arial',
 );
