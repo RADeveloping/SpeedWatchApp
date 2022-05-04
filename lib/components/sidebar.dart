1110e3658a49514b7d48ca95cdb2d2c561dbaaab
Sunwoo Park <swparkaust@me.com>
2022-05-04 14:23:29 -0700

    Set `resizeToAvoidBottomInset: false` in Scaffold

--- lib/components/sidebar.dart
+++ lib/components/sidebar.dart
@@ -24,6 +24,7 @@ class Sidebar extends GetView<SidebarController> {
           },
         ),
       ),
+      resizeToAvoidBottomInset: false,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
