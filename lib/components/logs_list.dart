763d9056ddd39dbea00b912654f97d86728ce4b8
Rahim Askarzadeh <raskarzadeh1@my.bcit.ca>
2022-05-05 14:14:49 -0700

    updated elipse button colour and only the ellipse button tappable

--- lib/components/logs_list.dart
+++ lib/components/logs_list.dart
@@ -39,10 +39,7 @@ class LogsList extends GetView<SidebarController> {
                             style: TextStyle(color: kColourPlaceHolderText),
                           ),
                         ),
-                        FaIcon(
-                          CupertinoIcons.ellipsis,
-                          color: kColourPlaceHolderText,
-                        ),
+                        LogsTileMoreButton(),
                       ],
                     ),
                     leading: Icon(
@@ -62,76 +59,6 @@ class LogsList extends GetView<SidebarController> {
                       ],
                       crossAxisAlignment: CrossAxisAlignment.start,
                     ),
-                    onPressed: (BuildContext context) {
-                      showPopover(
-                        context: context,
-                        backgroundColor: kColourRightPaneBackground,
-                        transitionDuration: const Duration(milliseconds: 150),
-                        bodyBuilder: (context) => Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            CupertinoButton(
-                              onPressed: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (_) => CameraCamera(
-                                              cameraSide: CameraSide.front,
-                                              onFile: (file) {
-                                                print(file);
-                                                //When take foto you should close camera
-                                                Navigator.pop(context);
-                                              },
-                                            )));
-                              },
-                              padding: EdgeInsets.symmetric(horizontal: 30),
-                              color: Colors.transparent,
-                              child: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  FaIcon(
-                                    CupertinoIcons.camera,
-                                    size: 32,
-                                  ),
-                                  SizedBox(
-                                    width: 12,
-                                  ),
-                                  Text('Take Photo')
-                                ],
-                              ),
-                            ),
-                            Container(
-                              width: 1,
-                              color: kColourTileDivider,
-                            ),
-                            CupertinoButton(
-                              onPressed: null,
-                              padding: EdgeInsets.symmetric(horizontal: 30),
-                              child: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  FaIcon(
-                                    CupertinoIcons.camera,
-                                    size: 32,
-                                  ),
-                                  SizedBox(
-                                    width: 12,
-                                  ),
-                                  Text('View Photo')
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                        direction: PopoverDirection.right,
-                        width: 400,
-                        height: 50,
-                        arrowHeight: 15,
-                        arrowWidth: 30,
-                      );
-                    },
                   ),
                   SettingsTile.navigation(
                     trailing: Row(
@@ -143,10 +70,7 @@ class LogsList extends GetView<SidebarController> {
                             style: TextStyle(color: kColourPlaceHolderText),
                           ),
                         ),
-                        FaIcon(
-                          CupertinoIcons.ellipsis,
-                          color: kColourPlaceHolderText,
-                        ),
+                        LogsTileMoreButton(),
                       ],
                     ),
                     leading: Icon(
@@ -166,9 +90,6 @@ class LogsList extends GetView<SidebarController> {
                       ],
                       crossAxisAlignment: CrossAxisAlignment.start,
                     ),
-                    onPressed: (BuildContext context) {
-                      Get.offAndToNamed('/session/445');
-                    },
                   ),
                   SettingsTile.navigation(
                     trailing: Row(
@@ -180,10 +101,7 @@ class LogsList extends GetView<SidebarController> {
                             style: TextStyle(color: kColourPlaceHolderText),
                           ),
                         ),
-                        FaIcon(
-                          CupertinoIcons.ellipsis,
-                          color: kColourPlaceHolderText,
-                        ),
+                        LogsTileMoreButton(),
                       ],
                     ),
                     leading: Icon(
@@ -203,9 +121,6 @@ class LogsList extends GetView<SidebarController> {
                       ],
                       crossAxisAlignment: CrossAxisAlignment.start,
                     ),
-                    onPressed: (BuildContext context) {
-                      Get.offAndToNamed('/session/445');
-                    },
                   ),
                   SettingsTile.navigation(
                     trailing: Row(
@@ -217,10 +132,7 @@ class LogsList extends GetView<SidebarController> {
                             style: TextStyle(color: kColourPlaceHolderText),
                           ),
                         ),
-                        FaIcon(
-                          CupertinoIcons.ellipsis,
-                          color: kColourPlaceHolderText,
-                        ),
+                        LogsTileMoreButton(),
                       ],
                     ),
                     leading: Icon(
@@ -240,7 +152,6 @@ class LogsList extends GetView<SidebarController> {
                       ],
                       crossAxisAlignment: CrossAxisAlignment.start,
                     ),
-                    onPressed: (BuildContext context) {},
                   ),
                 ],
               ),
@@ -248,6 +159,94 @@ class LogsList extends GetView<SidebarController> {
           ),
         ),
       ],
+    );
+  }
+}
+
+class LogsTileMoreButton extends StatelessWidget {
+  const LogsTileMoreButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        showPopover(
+          context: context,
+          backgroundColor: kColourRightPaneBackground,
+          transitionDuration: const Duration(milliseconds: 150),
+          bodyBuilder: (context) => Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CupertinoButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) => CameraCamera(
+                                cameraSide: CameraSide.front,
+                                onFile: (file) {
+                                  print(file);
+                                  //When take foto you should close camera
+                                  Navigator.pop(context);
+                                },
+                              )));
+                },
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                color: Colors.transparent,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    FaIcon(
+                      CupertinoIcons.camera,
+                      size: 32,
+                      color: kColourLight,
+                    ),
+                    SizedBox(
+                      width: 12,
+                    ),
+                    Text('Take Photo')
+                  ],
+                ),
+              ),
+              Container(
+                width: 1,
+                color: kColourTileDivider,
+              ),
+              CupertinoButton(
+                onPressed: null,
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    FaIcon(
+                      CupertinoIcons.photo,
+                      size: 32,
+                      // TODO UPDATE DISABLED COLOUR
+                    ),
+                    SizedBox(
+                      width: 12,
+                    ),
+                    Text('View Photo')
+                  ],
+                ),
+              ),
+            ],
+          ),
+          direction: PopoverDirection.right,
+          width: 400,
+          height: 50,
+          arrowHeight: 15,
+          arrowWidth: 30,
+        );
+      },
+      child: Icon(
+        CupertinoIcons.ellipsis,
+        color: kColourLight,
+      ),
     );
   }
 }
