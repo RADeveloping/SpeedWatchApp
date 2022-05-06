8d33e46cf721f9f1b7bbb075a3acf76b126f23b8
Jong Hwan Seo <jseo51@my.bcit.ca>
2022-05-05 18:25:04 -0700

    Created record_controller in order to implement getRecord() method for saving record to database.

---
+++ lib/controllers/record_controller.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:speedwatch/collections/record_collection.dart';
+import 'package:speedwatch/enums/speed_range.dart';
+import 'package:speedwatch/enums/vehicle_type.dart';
+
+class RecordController extends GetxController {
+  RecordCollection getRecord(SpeedRange speedRange, VehicleType vehicleType) {
+    return RecordCollection()
+        ..createdAt = DateTime.now()
+        ..speedRange = speedRange
+        ..vehicleType = vehicleType
+        ..session.value =
+  }
+}
