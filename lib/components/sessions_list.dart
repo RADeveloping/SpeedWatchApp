67720f7801d9c2ba37c55e240ba156cc58e90733
apsm100 <amritmanhas11@gmail.com>
2022-05-05 23:53:19 -0700

    Modularize session list

--- lib/components/sessions_list.dart
+++ lib/components/sessions_list.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:settings_ui/settings_ui.dart';
+import 'package:speedwatch/collections/session_collection.dart';
 import 'package:speedwatch/controllers/sidebar_controller.dart';
 
 import '../constants.dart';
@@ -14,7 +15,7 @@ class SessionsList extends GetView<SidebarController> {
       mainAxisSize: MainAxisSize.min,
       children: [
         Expanded(
-          child: SettingsList(
+          child: Obx(() => SettingsList(
             applicationType: ApplicationType.both,
             brightness: Brightness.light,
             lightTheme: SettingsThemeData(
@@ -24,235 +25,134 @@ class SessionsList extends GetView<SidebarController> {
               tileHighlightColor: kColourLight,
               dividerColor: kColourTileDivider,
             ),
-            sections: [
-              CustomSettingsSection(
-                child: Container(
-                  margin: EdgeInsetsDirectional.only(bottom: 10),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                          child: CupertinoSearchTextField(
-                            controller: controller.textController.value,
-                            itemColor: kColourLight,
-                            placeholder: 'Search Sessions',
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                            prefixInsets: EdgeInsets.all(10),
-                            onChanged: (String value) {
-                              print('The text has changed to: $value');
-                            },
-                            onSubmitted: (String value) {
-                              print('Submitted text: $value');
-                            },
-                            onSuffixTap: () {
-                              controller.textController.value.clear();
-                            },
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              CustomSettingsSection(
-                  child: Obx(() => SettingsSection(
-                        title: Text('FRIDAY, JANUARY 4, 2021'),
-                        tiles: controller.sessions.isNotEmpty ? controller.sessions
-                            .map((session) => SettingsTile.navigation(
-                                  title: Text(
-                                    session.streetAddress,
-                                    style: kTextStyleSidebarTile,
-                                  ),
-                                  value: Text('599'),
-                          onPressed: (BuildContext context) {
-                            Get.toNamed('/session/${session.id}');
-                          },
-                                ))
-                            .toList() : [SettingsTile.navigation(title: Text("oh no!"))],
-                      ))),
-              SettingsSection(
-                title: Text('THURSDAY, APRIL 28, 2022'),
-                tiles: <SettingsTile>[
-                  SettingsTile.navigation(
-                    title: Text(
-                      '164 Dunsmuir',
-                      style: kTextStyleSidebarTile,
-                    ),
-                    value: Text('1640'),
-                  ),
-                ],
-              ),
-              CustomSettingsSection(
-                child: CustomSettingsTile(
-                  child: GestureDetector(
-                    onTap: () => controller.archiveExpanded.value =
-                        !controller.archiveExpanded.value,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(right: 8),
-                                child: FaIcon(
-                                  CupertinoIcons.archivebox,
-                                  color: Colors.white,
-                                  size: 18,
-                                ),
-                              ),
-                              Text(
-                                'Archived',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16),
-                              ),
-                            ],
-                          ),
-                          Obx(() => Padding(
-                                padding: const EdgeInsets.only(right: 24),
-                                child: FaIcon(
-                                  controller.archiveExpanded.value
-                                      ? CupertinoIcons.chevron_right
-                                      : CupertinoIcons.chevron_down,
-                                  color: kColourLight,
-                                  size: 18 *
-                                      MediaQuery.of(context).textScaleFactor,
-                                ),
-                              )),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              CustomSettingsSection(
-                  child: Obx(() => controller.archiveExpanded.value
-                      ? SettingsSection(
-                          title: Text('FRIDAY, JANUARY 4, 2021'),
-                          tiles: <SettingsTile>[
-                            SettingsTile.navigation(
-                              title: Text(
-                                '985 Alberni St',
-                                style: kTextStyleSidebarTile,
-                              ),
-                              value: Text('599'),
-                              onPressed: (BuildContext context) {},
-                            ),
-                            SettingsTile.navigation(
-                              title: Text(
-                                'W47 and 16TH',
-                                style: kTextStyleSidebarTile,
-                              ),
-                              value: Text('1452'),
-                            ),
-                            SettingsTile.navigation(
-                              title: Text(
-                                '1967 Robson St',
-                                style: kTextStyleSidebarTile,
-                              ),
-                              value: Text('567'),
-                            ),
-                            SettingsTile.navigation(
-                              title: Text(
-                                '985 Alberni St',
-                                style: kTextStyleSidebarTile,
-                              ),
-                              value: Text('599'),
-                              onPressed: (BuildContext context) {},
-                            ),
-                            SettingsTile.navigation(
-                              title: Text(
-                                'W47 and 16TH',
-                                style: kTextStyleSidebarTile,
-                              ),
-                              value: Text('1452'),
-                            ),
-                            SettingsTile.navigation(
-                              title: Text(
-                                '1967 Robson St',
-                                style: kTextStyleSidebarTile,
-                              ),
-                              value: Text('567'),
-                            ),
-                            SettingsTile.navigation(
-                              title: Text(
-                                '985 Alberni St',
-                                style: kTextStyleSidebarTile,
-                              ),
-                              value: Text('599'),
-                              onPressed: (BuildContext context) {},
-                            ),
-                            SettingsTile.navigation(
-                              title: Text(
-                                'W47 and 16TH',
-                                style: kTextStyleSidebarTile,
-                              ),
-                              value: Text('1452'),
-                            ),
-                            SettingsTile.navigation(
-                              title: Text(
-                                '1967 Robson St',
-                                style: kTextStyleSidebarTile,
-                              ),
-                              value: Text('567'),
-                            ),
-                            SettingsTile.navigation(
-                              title: Text(
-                                '985 Alberni St',
-                                style: kTextStyleSidebarTile,
-                              ),
-                              value: Text('599'),
-                              onPressed: (BuildContext context) {},
-                            ),
-                            SettingsTile.navigation(
-                              title: Text(
-                                'W47 and 16TH',
-                                style: kTextStyleSidebarTile,
-                              ),
-                              value: Text('1452'),
-                            ),
-                            SettingsTile.navigation(
-                              title: Text(
-                                '1967 Robson St',
-                                style: kTextStyleSidebarTile,
-                              ),
-                              value: Text('567'),
-                            ),
-                            SettingsTile.navigation(
-                              title: Text(
-                                '985 Alberni St',
-                                style: kTextStyleSidebarTile,
-                              ),
-                              value: Text('599'),
-                              onPressed: (BuildContext context) {},
-                            ),
-                            SettingsTile.navigation(
-                              title: Text(
-                                'W47 and 16TH',
-                                style: kTextStyleSidebarTile,
-                              ),
-                              value: Text('1452'),
-                            ),
-                            SettingsTile.navigation(
-                              title: Text(
-                                '1967 Robson St',
-                                style: kTextStyleSidebarTile,
-                              ),
-                              value: Text('567'),
-                            ),
-                          ],
-                        )
-                      : Container()))
-            ],
-          ),
+            sections: buildList(context),
+          )),
         ),
       ],
     );
   }
+
+  List<AbstractSettingsSection> buildList(BuildContext context) {
+    List<AbstractSettingsSection> list = [searchBar(),
+      sessionListSection(controller.sessions, "ass", false),
+      archivedTab(context), sessionListSection(controller.sessions, "yo", true)
+    ];
+        return list;
+  }
+
+  CustomSettingsSection searchBar() {
+    return CustomSettingsSection(
+      child: Container(
+        margin: EdgeInsetsDirectional.only(bottom: 10),
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: CupertinoSearchTextField(
+                  controller: controller.textController.value,
+                  itemColor: kColourLight,
+                  placeholder: 'Search Sessions',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  prefixInsets: EdgeInsets.all(10),
+                  onChanged: (String value) {
+                    print('The text has changed to: $value');
+                  },
+                  onSubmitted: (String value) {
+                    print('Submitted text: $value');
+                  },
+                  onSuffixTap: () {
+                    controller.textController.value.clear();
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  CustomSettingsSection sessionListSection(List<SessionCollection> sessions, String date, bool archivable) {
+    return CustomSettingsSection(
+        child: !archivable ? SettingsSection(
+          title: Text(date),
+          tiles: sessions.isNotEmpty ? sessions
+              .map((session) => sessionListItem(session))
+              .toList() : [SettingsTile.navigation(title: Text("oh no!"))],
+        ):
+        controller.archiveExpanded.value ? SettingsSection(
+    title: Text(date),
+    tiles: sessions.isNotEmpty ? sessions
+        .map((session) => sessionListItem(session))
+        .toList() : [SettingsTile.navigation(title: Text("oh no!"))] ) : Container());
+  }
+
+
+
+  SettingsTile sessionListItem(SessionCollection session) {
+    return SettingsTile.navigation(
+      title: Text(
+        session.streetAddress,
+        style: kTextStyleSidebarTile,
+      ),
+      value: Text('599'),
+      onPressed: (BuildContext context) {
+        Get.toNamed('/session/${session.id}');
+      },
+    );
+  }
+
+  CustomSettingsSection archivedTab(BuildContext context) {
+    return CustomSettingsSection(
+      child: CustomSettingsTile(
+        child: GestureDetector(
+          onTap: () => controller.archiveExpanded.value =
+          !controller.archiveExpanded.value,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 8),
+                      child: FaIcon(
+                        CupertinoIcons.archivebox,
+                        color: Colors.white,
+                        size: 18,
+                      ),
+                    ),
+                    Text(
+                      'Archived',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16),
+                    ),
+                  ],
+                ),
+                Obx(() => Padding(
+                  padding: const EdgeInsets.only(right: 24),
+                  child: FaIcon(
+                    controller.archiveExpanded.value
+                        ? CupertinoIcons.chevron_right
+                        : CupertinoIcons.chevron_down,
+                    color: kColourLight,
+                    size: 18 *
+                        MediaQuery.of(context).textScaleFactor,
+                  ),
+                )),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+
 }
