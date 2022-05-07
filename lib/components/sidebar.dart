dffd7dddb119478c6eef73574136f015875c0acf
Rahim Askarzadeh <raskarzadeh1@my.bcit.ca>
2022-05-07 15:37:50 -0700

    Fixed the tap touch area for the more button on export

--- lib/components/sidebar.dart
+++ lib/components/sidebar.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:get/get.dart';
-import 'package:share_plus/share_plus.dart';
 import 'package:speedwatch/constants.dart';
 import 'package:speedwatch/controllers/sidebar_controller.dart';
 
@@ -30,26 +29,22 @@ class Sidebar extends GetView<SidebarController> {
       trailing: Obx(() => CupertinoButton(
             padding: EdgeInsets.zero,
             child: controller.isEditMode.value
-                ? GestureDetector(
-                    child: Text(
-                      'Done',
-                      style: TextStyle(color: kColourLight),
-                    ),
-                    onTap: () {
-                      controller.isEditMode.value = false;
-                    },
+                ? Text(
+                    'Done',
+                    style: TextStyle(color: kColourLight),
                   )
-                : GestureDetector(
-                    child: Icon(
-                      CupertinoIcons.ellipsis_circle,
-                      color: kColourLight,
-                    ),
-                    onTapDown: (tapDetails) {
-                      controller.isEditMode.value = true;
-                      controller.selectedSessions().clear();
-                    },
+                : Icon(
+                    CupertinoIcons.ellipsis_circle,
+                    color: kColourLight,
                   ),
-            onPressed: () {},
+            onPressed: () {
+              if (controller.isEditMode.value) {
+                controller.isEditMode.value = false;
+              } else {
+                controller.isEditMode.value = true;
+                controller.selectedSessions().clear();
+              }
+            },
           )),
       heroTag: 0,
       child: child,
