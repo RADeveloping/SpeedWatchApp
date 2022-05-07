c063b1e3654b81e9dc4120d622339d7d2562ccd8
Rahim Askarzadeh <raskarzadeh1@my.bcit.ca>
2022-05-07 00:33:41 -0700

    - Added the export function to the list page

--- lib/components/sidebar.dart
+++ lib/components/sidebar.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:get/get.dart';
-import 'package:positioned_tap_detector_2/positioned_tap_detector_2.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:speedwatch/constants.dart';
 import 'package:speedwatch/controllers/sidebar_controller.dart';
@@ -14,8 +13,6 @@ class Sidebar extends GetView<SidebarController> {
   final String largeTitle;
   final Widget? leading;
   final String? previousPageTitle;
-  Rx<TapPosition> position =
-      TapPosition(Offset(0.0, 0.0), Offset(0.0, 0.0)).obs;
 
   Sidebar(
       {required this.child,
@@ -30,24 +27,30 @@ class Sidebar extends GetView<SidebarController> {
       backgroundColor: kColourSidebarBackground,
       largeTitle: largeTitle,
       previousPageTitle: previousPageTitle,
-      trailing: CupertinoButton(
-        padding: EdgeInsets.zero,
-        child: PositionedTapDetector2(
-          child: Icon(
-            CupertinoIcons.share_up,
-            color: kColourLight,
-          ),
-          onTap: (positioned) {
-            // controller.isEditMode.value = !controller.isEditMode.value;
-            Share.share(
-              'LOG NAME WOULD GO HERE',
-              sharePositionOrigin: Rect.fromLTWH(
-                  positioned.global.dx, positioned.global.dy, 0.5, 20),
-            );
-          },
-        ),
-        onPressed: () {},
-      ),
+      trailing: Obx(() => CupertinoButton(
+            padding: EdgeInsets.zero,
+            child: controller.isEditMode.value
+                ? GestureDetector(
+                    child: Text(
+                      'Done',
+                      style: TextStyle(color: kColourLight),
+                    ),
+                    onTap: () {
+                      controller.isEditMode.value = false;
+                    },
+                  )
+                : GestureDetector(
+                    child: Icon(
+                      CupertinoIcons.ellipsis_circle,
+                      color: kColourLight,
+                    ),
+                    onTapDown: (tapDetails) {
+                      controller.isEditMode.value = true;
+                      controller.selectedSessions().clear();
+                    },
+                  ),
+            onPressed: () {},
+          )),
       heroTag: 0,
       child: child,
     );
