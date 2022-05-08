ecb053b2d454d54323ace07f4161350fea9bb4b9
apsm100 <amritmanhas11@gmail.com>
2022-05-08 13:16:10 -0700

    Add excel and initial export service

---
+++ lib/services/export_service.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:excel/excel.dart';
+import 'package:flutter/services.dart' show ByteData, rootBundle;
+
+class ExportService {
+  Future<Excel> getExcelTemplate() async {
+    ByteData data = await rootBundle.load("assets/existing_excel_file.xlsx");
+    var bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+    var excel = Excel.decodeBytes(bytes);
+
+    for (var table in excel.tables.keys) {
+      print(table); //sheet Name
+      if (excel.tables[table]?.rows != null) {
+        for (var row in excel.tables[table]!.rows) {
+          print("$row");
+        }
+      }
+    }
+
+    return excel;
+  }
+}
