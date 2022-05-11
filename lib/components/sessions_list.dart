54b2856884780b0e013166de7f45450aa79d16ab
Sunwoo Park <swparkaust@me.com>
2022-05-11 14:37:05 -0700

    Add empty state for sessions

--- lib/components/sessions_list.dart
+++ lib/components/sessions_list.dart
@@ -21,18 +21,28 @@ class SessionsList extends GetView<SidebarController> {
       mainAxisSize: MainAxisSize.min,
       children: [
         Expanded(
-          child: Obx(() => SettingsList(
-                applicationType: ApplicationType.both,
-                brightness: Brightness.light,
-                lightTheme: SettingsThemeData(
-                  settingsListBackground: kColourSidebarBackground,
-                  settingsSectionBackground: kColourSidebarTile,
-                  settingsTileTextColor: kColourSidebarTileText,
-                  tileHighlightColor: kColourLight,
-                  dividerColor: kColourTileDivider,
-                ),
-                sections: buildList(context),
-              )),
+          child: Obx(() => controller.sessions.isEmpty
+              ? Center(
+                  child: Text(
+                    'No Sessions',
+                    style: TextStyle(
+                        color: kColourPlaceHolderText,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold),
+                  ),
+                )
+              : SettingsList(
+                  applicationType: ApplicationType.both,
+                  brightness: Brightness.light,
+                  lightTheme: SettingsThemeData(
+                    settingsListBackground: kColourSidebarBackground,
+                    settingsSectionBackground: kColourSidebarTile,
+                    settingsTileTextColor: kColourSidebarTileText,
+                    tileHighlightColor: kColourLight,
+                    dividerColor: kColourTileDivider,
+                  ),
+                  sections: buildList(context),
+                )),
         ),
       ],
     );
