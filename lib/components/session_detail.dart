5c3e7efd791a0104725645dd4417ab9b6a19c8ee
Jong Hwan Seo <jseo51@my.bcit.ca>
2022-05-13 12:47:17 -0700

    Took out CupertinoButton widgets from the undo and redo buttons and added a sizedbox in between.

--- lib/components/session_detail.dart
+++ lib/components/session_detail.dart
@@ -34,49 +34,43 @@ class SessionDetail extends GetView<SessionDetailDetailController> {
         trailing: Row(
           mainAxisSize: MainAxisSize.min,
           children: [
-            CupertinoButton(
-              // Undo
-              onPressed: () {},
-              child: GestureDetector(
-                child: Obx(() => Icon(
-                      CupertinoIcons.arrow_uturn_left_circle,
-                      color:
-                          s.records.length > 0 && s.isSessionCompleted.isFalse
-                              ? kColourLight
-                              : kColourDisabledButton,
-                    )),
-                onTap: () {
-                  DbService dbService = Get.find();
-                  List<RecordCollection> records = s.records.value;
-                  records.length == 0 || s.isSessionCompleted.isTrue
-                      ? null
-                      : dbService.deleteLatestRecord(records[0]);
-                },
-              ),
-              padding: EdgeInsets.zero,
-            ),
-            CupertinoButton(
-                // Redo
-                onPressed: () {},
-                child: GestureDetector(
-                  child: Obx(() => Icon(
-                        CupertinoIcons.arrow_uturn_right_circle,
-                        color: s.deletedRecords.length > 0 &&
-                                s.isSessionCompleted.isFalse
+            GestureDetector(
+              child: Obx(() => Icon(
+                    CupertinoIcons.arrow_uturn_left_circle,
+                    color:
+                        s.records.length > 0 && s.isSessionCompleted.isFalse
                             ? kColourLight
                             : kColourDisabledButton,
-                      )),
-                  onTap: () {
-                    DbService dbService = Get.find();
-                    List<RecordCollection> deletedRecords =
-                        s.deletedRecords.value;
-                    deletedRecords.length == 0 || s.isSessionCompleted.isTrue
-                        ? null
-                        : dbService
-                            .restoreLatestDeletedRecord(deletedRecords[0]);
-                  },
-                ),
-                padding: EdgeInsets.zero),
+                  )),
+              onTap: () {
+                DbService dbService = Get.find();
+                List<RecordCollection> records = s.records.value;
+                records.length == 0 || s.isSessionCompleted.isTrue
+                    ? null
+                    : dbService.deleteLatestRecord(records[0]);
+              },
+            ),
+            SizedBox(
+              width: 12,
+            ),
+            GestureDetector(
+              child: Obx(() => Icon(
+                    CupertinoIcons.arrow_uturn_right_circle,
+                    color: s.deletedRecords.length > 0 &&
+                            s.isSessionCompleted.isFalse
+                        ? kColourLight
+                        : kColourDisabledButton,
+                  )),
+              onTap: () {
+                DbService dbService = Get.find();
+                List<RecordCollection> deletedRecords =
+                    s.deletedRecords.value;
+                deletedRecords.length == 0 || s.isSessionCompleted.isTrue
+                    ? null
+                    : dbService
+                        .restoreLatestDeletedRecord(deletedRecords[0]);
+              },
+            ),
           ],
         ),
         backgroundColor: kColourRightPaneBackground,
